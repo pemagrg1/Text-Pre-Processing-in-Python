ce6f8eebd6c4a56f6e05d0ef09062604133f257e
Pema Gurung <30492527+pemagrg1@users.noreply.github.com>
2018-10-31 17:44:54 +0545

    Update to_lower.py

--- to_lower.py
+++ to_lower.py
@@ -1,4 +1,6 @@
+# coding: utf-8
 import nltk
+from nltk import word_tokenize
 
 def to_lower(text):
     """
@@ -7,5 +9,11 @@ def to_lower(text):
     return ' '.join([w.lower() for w in word_tokenize(text)])
 
 
-text = """Harry Potter is the most miserable, lonely boy you can imagine. He's shunned by his relatives, the Dursley's, that have raised him since he was an infant. He's forced to live in the cupboard under the stairs, forced to wear his cousin Dudley's hand-me-down clothes, and forced to go to his neighbour's house when the rest of the family is doing something fun. Yes, he's just about as miserable as you can get."""
-print (to_lower(text))
+def main():
+    text = """Harry Potter is the most miserable, lonely boy you can imagine. He’s shunned by his relatives, the Dursley’s, that have raised him since he was an infant. He’s forced to live in the cupboard under the stairs, forced to wear his cousin Dudley’s hand-me-down clothes, and forced to go to his neighbour’s house when the rest of the family is doing something fun. Yes, he’s just about as miserable as you can get."""
+    lower_text = to_lower(text)
+    print (lower_text)
+
+if __name__ == '__main__':
+    main()
+    
