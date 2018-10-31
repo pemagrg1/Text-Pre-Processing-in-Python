40a50963d996d2342b580521c01775f8d4bf2fcb
Pema Gurung <30492527+pemagrg1@users.noreply.github.com>
2018-10-31 17:57:51 +0545

    Create Stemming_text.py

---
+++ Stemming_text.py
@@ -0,0 +1,25 @@
+# coding: utf-8
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import SnowballStemmer
+
+stopword = stopwords.words('english')
+snowball_stemmer = SnowballStemmer('english')
+
+
+def stemming(text):
+    """
+    take string input and stem the words.
+    use snowball_stemmer to stem the string.
+    """
+    word_tokens = nltk.word_tokenize(text)
+    stemmed_word = [snowball_stemmer.stem(word) for word in word_tokens]
+    return (stemmed_word)
+    
+def main():
+    text = "the functions of this fan is awesome"
+    print (remove_punct(text))
+
+if __name__ == '__main__':
+    main()
+    
