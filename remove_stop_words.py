27fb697bff5aaa458b8002badf7ccb2f34116532
Pema Gurung <30492527+pemagrg1@users.noreply.github.com>
2018-10-31 13:27:12 +0545

    Update remove_stop_words.py

--- remove_stop_words.py
+++ remove_stop_words.py
@@ -1,9 +1,7 @@
 from nltk import word_tokenize
 from nltk.corpus import stopwords
 
-
 stop_words = set(stopwords.words('english'))
-
 
 def remove_stopwords(sentence):
     #removes all the stop words like "is,the,a, etc."
@@ -14,6 +12,7 @@ def remove_stopwords(sentence):
     return " ".join(clean_sent)
 #     return ' '.join([w for w in word_tokenize(sentence) if not w in stop_words]) #5 lines of code can be written in one line
 
+##########DEMO#########
 text = """Harry Potter is the most miserable, lonely boy you can imagine. He’s shunned by his relatives, the Dursley’s, that have raised him since he was an infant. He’s forced to live in the cupboard under the stairs, forced to wear his cousin Dudley’s hand-me-down clothes, and forced to go to his neighbour’s house when the rest of the family is doing something fun. Yes, he’s just about as miserable as you can get."""
 remove_stopwords(text)
 
