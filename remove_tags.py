c0040c4390e2956b3aa5b98a15806d7ec4dcc4b0
Pema Gurung <30492527+pemagrg1@users.noreply.github.com>
2018-10-31 17:53:33 +0545

    Create remove_tags.py

---
+++ remove_tags.py
@@ -0,0 +1,19 @@
+# coding: utf-8
+import nltk
+from nltk import word_tokenize
+
+def remove_Tags(text):
+    """
+    take string input and clean string without tags.
+    use regex to remove the html tags.
+    """
+    cleaned_text = re.sub('<[^<]+?>','', text)
+    return cleaned_text
+    
+def main():
+    text = """<head><body>hello world!</body></head>"""
+    print (remove_Tags(text))
+
+if __name__ == '__main__':
+    main()
+    
