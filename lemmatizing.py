a48ed32f6da5e13d858005233d03fcbbefd0a2bc
Pema Gurung <30492527+pemagrg1@users.noreply.github.com>
2018-10-31 17:59:13 +0545

    Create lemmatizing.py

---
+++ lemmatizing.py
@@ -0,0 +1,24 @@
+# coding: utf-8
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+
+stopword = stopwords.words('english')
+wordnet_lemmatizer = WordNetLemmatizer()
+
+def lemmatize(text):
+    """
+    take string input and lemmatize the words.
+    use WordNetLemmatizer to lemmatize the words.
+    """
+    word_tokens = nltk.word_tokenize(text)
+    lemmatized_word = [wordnet_lemmatizer.lemmatize(word) for word in word_tokens]
+    return (lemmatized_word)
+
+def main():
+    text = "the functions of this fan is awesome"
+    print (remove_punct(text))
+
+if __name__ == '__main__':
+    main()
+    
