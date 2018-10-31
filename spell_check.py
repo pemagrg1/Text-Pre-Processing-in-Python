529a01f37c7a84980c956a8579ff56ddf92fcab4
Pema Gurung <30492527+pemagrg1@users.noreply.github.com>
2018-10-31 17:48:28 +0545

    Create spell_check.py

---
+++ spell_check.py
@@ -0,0 +1,21 @@
+# coding: utf-8
+import nltk
+from autocorrect import spell
+from nltk import word_tokenize
+
+def autospell(text):
+    """
+    correct the spelling of the word.
+    """
+    spells = [spell(w) for w in (nltk.word_tokenize(text))]
+    return spells
+
+
+def main():
+    text = "This is a wrld of hope"
+    spell_text = autospell(text)
+    print (spell_text)
+
+if __name__ == '__main__':
+    main()
+    
